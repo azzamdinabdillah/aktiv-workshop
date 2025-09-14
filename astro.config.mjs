5b87a57d2cd8481e6abb769a1d0cc214e88bfbeb
Azam Din Abdillah <azzamdinabdillah123@gmail.com>
2025-09-14 11:43:23 +0700

    feat : workshop card component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     assetsPrefix: "/aktiv",
-    format: "preserve"
+    format: "preserve",
   },
   base: "/aktiv",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    define: {
+      "import.meta.env.ICONS_FOLDER": JSON.stringify("/aktiv/assets/icons"),
+    },
+  },
 });
