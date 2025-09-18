f71e445fb2a0cf9e8fd39cd1f7b250311c868a30
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2025-09-18 10:47:13 +0700

    fix : env, script read more, dll

--- src/scripts/read-more-description.js
+++ src/scripts/read-more-description.js
@@ -2,15 +2,17 @@ const descriptionText = document.getElementById("description-text");
 const readMoreBtn = document.getElementById("read-more-btn");
 
 // Add line clamp initially
-descriptionText.classList.add("line-clamp-4");
+if (descriptionText) {
+  descriptionText.classList.add("line-clamp-4");
+  // Add click event listener
+  readMoreBtn.addEventListener("click", function () {
+    if (descriptionText.classList.contains("line-clamp-4")) {
+      descriptionText.classList.remove("line-clamp-4");
+      readMoreBtn.textContent = "Read less...";
+    } else {
+      descriptionText.classList.add("line-clamp-4");
+      readMoreBtn.textContent = "Read more...";
+    }
+  });
+}
 
-// Add click event listener
-readMoreBtn.addEventListener("click", function () {
-  if (descriptionText.classList.contains("line-clamp-4")) {
-    descriptionText.classList.remove("line-clamp-4");
-    readMoreBtn.textContent = "Read less...";
-  } else {
-    descriptionText.classList.add("line-clamp-4");
-    readMoreBtn.textContent = "Read more...";
-  }
-});
